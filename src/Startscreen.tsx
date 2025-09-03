9d149bda3a4708221fc7b4dbf93d3a5c35d40fe6
Vivian Speijer <vivianspeijer@gmail.com>
2025-09-03 20:34:00 +0200

    remove bot

--- src/Startscreen.tsx
+++ src/Startscreen.tsx
@@ -55,35 +55,31 @@ export function StartScreen() {
 						<img src={logo} />
 					</div>
 					<div id="chat">
-						{tourney.chat
-							.filter((msg: any) => msg.team !== "bot")
-							.map((msg: any, idx: number) => (
-								<motion.div
-									className="chat-message"
-									key={msg.id ?? idx}
-									initial={{ opacity: 0, y: 20 }}
-									animate={{ opacity: 1, y: 0 }}
-									transition={{ duration: 0.5 }}
-									layout
+						{tourney.chat.map((msg: any, idx: number) => (
+							<motion.div
+								className="chat-message"
+								key={msg.id ?? idx}
+								initial={{ opacity: 0, y: 20 }}
+								animate={{ opacity: 1, y: 0 }}
+								transition={{ duration: 0.24 }}
+								layout
+							>
+								<div
+									className="chat-username"
+									style={{
+										color:
+											msg.team === "left"
+												? "#FD515C"
+												: msg.team === "right"
+												? "#5583F9"
+												: "#FF962D"
+									}}
 								>
-									<div
-										className="chat-username"
-										style={{
-											color:
-												msg.team === "left"
-													? "#FD515C"
-													: msg.team === "right"
-													? "#5583F9"
-													: "#FF962D"
-										}}
-									>
-										{msg.name}:
-									</div>
-									<div className="chat-text">
-										{msg.message}
-									</div>
-								</motion.div>
-							))}
+									{msg.name}:
+								</div>
+								<div className="chat-text">{msg.message}</div>
+							</motion.div>
+						))}
 					</div>
 					<div id="casters">CASTERS</div>
 				</div>
