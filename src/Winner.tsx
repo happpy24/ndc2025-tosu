ec22e4750b812dc922c42b3d38e38995f11c1aa3
Vivian Speijer <vivianspeijer@gmail.com>
2025-09-03 18:23:38 +0200

    chat function, startscreen almost done

--- src/Winner.tsx
+++ src/Winner.tsx
@@ -2,6 +2,7 @@ import { useTosu } from "./state/tosu";
 import "./static/style.css";
 import avatar from "./static/img/happy.png";
 import logo from "./static/img/logo.png";
+import { motion } from "motion/react";
 
 export function WinnerScreen() {
 	const { player1, player2, tourney, beatmap, directPath } = useTosu();
@@ -14,7 +15,37 @@ export function WinnerScreen() {
 					<div id="ndc-logo">
 						<img src={logo} />
 					</div>
-					<div id="chat"></div>
+					<div id="chat">
+						{tourney.chat
+							.filter((msg: any) => msg.team !== "bot")
+							.map((msg: any, idx: number) => (
+								<motion.div
+									className="chat-message"
+									key={msg.id ?? idx}
+									initial={{ opacity: 0, y: 20 }}
+									animate={{ opacity: 1, y: 0 }}
+									transition={{ duration: 0.5 }}
+									layout
+								>
+									<div
+										className="chat-username"
+										style={{
+											color:
+												msg.team === "left"
+													? "#FD515C"
+													: msg.team === "right"
+													? "#5583F9"
+													: "#FF962D"
+										}}
+									>
+										{msg.name}:
+									</div>
+									<div className="chat-text">
+										{msg.message}
+									</div>
+								</motion.div>
+							))}
+					</div>
 					<div id="casters">CASTERS</div>
 				</div>
 			</div>
