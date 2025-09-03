57bda401b0ea5bcdb3932c7baab8db427bb577a0
Vivian Speijer <vivianspeijer@gmail.com>
2025-09-03 15:50:22 +0200

    State changer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { VersusScreen } from "./Versus";
 import { TosuProvider } from "./state/tosu";
 
+var state = 2;
+if (state === 0) {
+	var loadstate = <div>StartScreen</div>;
+} else if (state === 1) {
+	var loadstate = <div>StandbyScreen</div>;
+} else if (state === 2) {
+	var loadstate = <VersusScreen />;
+} else if (state === 3) {
+	var loadstate = <div>MappoolScreen</div>;
+} else if (state === 4) {
+	var loadstate = <div>SchedulingScreeen</div>;
+} else if (state === 5) {
+	var loadstate = <div>WinnerScreen</div>;
+}
+
 export function App() {
-	return (
-		<TosuProvider>
-			<VersusScreen />
-		</TosuProvider>
-	);
+	return <TosuProvider>{loadstate}</TosuProvider>;
 }
