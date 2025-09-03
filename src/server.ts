04b0c222ab83c8704913d55b9545b7bcf6dbeed1
Bas van den Wollenberg <hi@bas.sh>
2025-09-03 10:09:35 +0200

    add simple rebroadcasting websocket server

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,26 @@ const server = serve({
   routes: {
     "/*": index,
   },
+  fetch(req, server) {
+    if (server.upgrade(req)) {
+      return;
+    }
+    return new Response("websocket upgrade error", { status: 400 });
+  },
+  websocket: {
+    open(ws) {
+      ws.subscribe("messages");
+      console.log("client connected to websocket");
+    },
+    message(ws, message) {
+      // rebroadcast incoming messages to all clients https://bun.com/docs/api/websockets#pub-sub
+      server.publish("messages", message);
+    },
+    close(ws) {
+      ws.unsubscribe("messages");
+      console.log("client disconnected from websocket");
+    },
+  },
   development: process.env.NODE_ENV !== "production" && {
     hmr: true,
     console: true,
