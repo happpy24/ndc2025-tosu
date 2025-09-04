55d281f795a8e244eb9bc5ee3f8ce4af455462d8
Bas van den Wollenberg <hi@bas.sh>
2025-09-04 02:47:19 +0200

    update startscreen with huis data

--- src/Startscreen.tsx
+++ src/Startscreen.tsx
@@ -1,43 +1,52 @@
 import { useTosu } from "./state/tosu";
 import "./static/style.css";
-import avatar from "./static/img/happy.png";
 import logo from "./static/img/logo.png";
 import { motion } from "motion/react";
+import { useMatchQuery } from "./state/huis";
 
 export function StartScreen() {
-  const { player1, player2, tourney, beatmap } = useTosu();
+  const { tourney, beatmap } = useTosu();
+  const { player1, player2, ...match } = useMatchQuery();
 
   return (
     <div>
       <div id="main">
         <div id="ss-top">
           <div id="ss-title">
-            <div id="ss-stage-name">Quarter Finals</div>
-            <div id="ss-winner-loser">(Winners Bracket)</div>
+            <div id="ss-stage-name">{match.roundName}</div>
+            <div id="ss-winner-loser">({match.bracket} Bracket)</div>
           </div>
           <div id="ss-upcoming">UPCOMING</div>
           <div id="ss-vs">
             <div id="ss-red-player">
               <div id="ss-red-player-icon">
-                <img src={avatar} />
+                <img src={player1.avatarUrl} />
               </div>
               <div id="ss-player-info" className="align-right">
                 <div id="ss-player-name">{player1.name}</div>
-                <div id="ss-player-seed">Seed: 22</div>
-                <div id="ss-player-supporters">Supporters: 22</div>
-                <div id="ss-player-pickems">Pickems: 50%</div>
+                <div id="ss-player-seed">Seed: {player1.seed}</div>
+                <div id="ss-player-supporters">
+                  Supporters: {player1.supporters}
+                </div>
+                <div id="ss-player-pickems">
+                  Pickems: {player1.pickemsRate}%
+                </div>
               </div>
             </div>
             <div id="ss-vs-text">VS</div>
             <div id="ss-blue-player">
               <div id="ss-blue-player-icon">
-                <img src={avatar} />
+                <img src={player2.avatarUrl} />
               </div>
               <div id="ss-player-info">
                 <div id="ss-player-name">{player2.name}</div>
-                <div id="ss-player-seed">Seed: 22</div>
-                <div id="ss-player-supporters">Supporters: 22</div>
-                <div id="ss-player-pickems">Pickems: 50%</div>
+                <div id="ss-player-seed">Seed: {player2.seed}</div>
+                <div id="ss-player-supporters">
+                  Supporters: {player2.supporters}
+                </div>
+                <div id="ss-player-pickems">
+                  Pickems: {player2.pickemsRate}%
+                </div>
               </div>
             </div>
           </div>
