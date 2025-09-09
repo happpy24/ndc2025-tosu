0a73ac1505efd7364867f64779416a3056cb6845
Bas van den Wollenberg <hi@bas.sh>
2025-09-09 23:00:27 +0200

    update build config
    added some extra info that will be useful later and commented out the Tailwind plugin as we're not using it yet anyway

--- build.ts
+++ build.ts
@@ -1,19 +1,39 @@
+import { $ } from "bun";
 import bunPluginTailwind from "bun-plugin-tailwind";
 
-const targets = {
+const targets: Record<string, Bun.Build.Target> = {
   windows: "bun-windows-x64",
   macos: "bun-darwin-x64",
   linux: "bun-linux-x64",
-} satisfies Record<string, Bun.Build.Target>;
+};
+
+const version = await $`git describe --tags --always`.text();
+const buildTime = new Date().toISOString();
+const gitCommit = await $`git rev-parse HEAD`.text();
+
+const define = (config: Record<string, string>) =>
+  Object.fromEntries(
+    Object.entries(config).map(([key, value]) => [key, JSON.stringify(value)]),
+  );
 
 for (const [platform, target] of Object.entries(targets)) {
   await Bun.build({
-    plugins: [bunPluginTailwind],
+    // plugins: [bunPluginTailwind],
     entrypoints: ["./src/server.ts"],
     outdir: "./dist",
+    env: "PUBLIC_*",
+    define: define({
+      BUILD_VERSION: version.trim(),
+      BUILD_TIME: buildTime,
+      GIT_COMMIT: gitCommit.trim(),
+    }),
     compile: {
       target,
       outfile: `ndc-overlay-${platform}`,
     },
+    minify: true,
+    sourcemap: true,
   });
 }
+
+console.log("Build successful :3");
